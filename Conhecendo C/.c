b609bc8dd0f62ccfbe4251b48b1186f8a663c863
Grazi <grazielli_azevedo@hotmail.com>
2023-08-02 23:20:53 -0300

    Commit

--- Conhecendo C/.c
+++ Conhecendo C/.c
@@ -1,4 +1,17 @@
 #include <stdlib.h>
+
 void main() {
     int valor1, valor2, soma;
 }
+
+printf("expressão do controle", listas de argumentos);
+printf("o valor encontrado foi %c",valor1); // Permite a escrita de apenas um caractere]
+printf("o valor encontrado foi %d",valor1); // Permite a escrita de números inteiros decimais]
+printf("o valor encontrado foi %e",valor1); // Realiza-se a escrita de números em notção cientifica] 
+printf("o valor encontrado foi %f",valor1); // É feita a escrita de números reais (ponto flutuante)]
+printf("o valor encontrado foi %s",valor1); // Efetua-se a escrita de uma série de caracteres]
+
+scanf("expressão de controle", lista de argumentos);
+scanf("%d",&valor);
+
+main( )
